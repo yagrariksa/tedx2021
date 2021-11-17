7c12f9fa64cfade92bf12f1cde9cf10edd075e4f
adiharka <andimahardika73@gmail.com>
2021-11-17 14:55:23 +0700

    Admin minor fix

--- resources/views/admin/main/participant.blade.php
+++ resources/views/admin/main/participant.blade.php
@@ -20,12 +20,16 @@
                     <h4>All Participant</h4>
                     <div class="card-header-form">
                         <div class="input-group">
+                            <button onclick="window.open('{{route('admin.main.excel')}}')" class="btn btn-success mr-3">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-earmark-spreadsheet" viewBox="0 0 16 16">
+                                    <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2zM9.5 3A1.5 1.5 0 0 0 11 4.5h2V9H3V2a1 1 0 0 1 1-1h5.5v2zM3 12v-2h2v2H3zm0 1h2v2H4a1 1 0 0 1-1-1v-1zm3 2v-2h3v2H6zm4 0v-2h3v1a1 1 0 0 1-1 1h-2zm3-3h-3v-2h3v2zm-7 0v-2h3v2H6z"/>
+                                  </svg>
+                                Download Excel</button>
                             <input type="text" class="form-control" id="table-search" onkeyup="search()"
                                 placeholder="Search name">
                             <div class="input-group-btn">
                                 <button class="btn btn-primary"><i class="fas fa-search"></i></button>
                             </div>
-                            <button onclick="window.open('{{route('admin.main.excel')}}')" class="btn btn-dark">Excel</button>
                         </div>
                     </div>
                 </div>
@@ -49,14 +53,14 @@
                                         <td>{{ $d->name }}</td>
                                         <td>{{ $d->email }}</td>
                                         <td>
-                                            <button class="btn btn-warning" id="detail-item" 
+                                            <button class="btn btn-warning" id="detail-item"
                                                 data-toggle ="modal"
-                                                data-target ="#infoModal" 
-                                                data-name ='{{ $d->name }}' 
-                                                data-email ='{{ $d->email }}' 
-                                                data-age ='{{ $d->age }}' 
-                                                data-phone ='{{ $d->phone }}' 
-                                                data-institute ='{{ $d->institute }}' 
+                                                data-target ="#infoModal"
+                                                data-name ='{{ $d->name }}'
+                                                data-email ='{{ $d->email }}'
+                                                data-age ='{{ $d->age }}'
+                                                data-phone ='{{ $d->phone }}'
+                                                data-institute ='{{ $d->institute }}'
                                                 data-address ="{{ $d->address }}">Lihat Detail</button>
                                         </td>
                                     </tr>
@@ -65,7 +69,7 @@
                         </table>
                     </div>
                 </div>
-                <div class="card-footer text-right">
+                {{-- <div class="card-footer text-right">
                     <nav class="d-inline-block">
                         <ul class="pagination mb-0" id="ul-pagination">
                             <li class="page-item disabled">
@@ -82,7 +86,7 @@
                             </li>
                         </ul>
                     </nav>
-                </div>
+                </div> --}}
             </div>
         </div>
     </div>
@@ -173,7 +177,7 @@
             $('#info-institute').text($(this).data("institute"));
             $('#info-address').text($(this).data("address"));
         });
-        
+
 
         function search() {
             // Declare variables
